9f3b847adf92d9b6b420298955420fca64f379d9
Alex Biehl <biehl.al@gmail.com>
2024-08-06 19:24:46 -0400

    started creating plumbing to auto-update ACLs when a domain object is created/deleted

--- src/main/java/com/alexbiehl/demo/repository/eventhandler/WidgetEventHandler.java
+++ src/main/java/com/alexbiehl/demo/repository/eventhandler/WidgetEventHandler.java
@@ -1,14 +1,21 @@
 package com.alexbiehl.demo.repository.eventhandler;
 
 import com.alexbiehl.demo.model.Widget;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 
 @RepositoryEventHandler
 public class WidgetEventHandler {
 
-    @HandleBeforeCreate
-    public void handleWidgetCreate(Widget w) {
+    @HandleAfterCreate
+    public void handleAfterCreate(Widget w) {
+
+    }
+
+    @HandleAfterDelete
+    public void handleAfterDelete(Widget w) {
 
     }
 }
--- src/main/java/com/alexbiehl/demo/service/PermissionService.java
+++ src/main/java/com/alexbiehl/demo/service/PermissionService.java
@@ -1,9 +1,36 @@
 package com.alexbiehl.demo.service;
 
+import com.alexbiehl.demo.model.Widget;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.model.MutableAcl;
+import org.springframework.security.acls.model.MutableAclService;
+import org.springframework.security.acls.model.NotFoundException;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PermissionService {
 
+        @Autowired
+        private MutableAclService aclService;
 
+        public void handleCreate(Widget w) {
+            ObjectIdentityImpl oid = new ObjectIdentityImpl(w);
+
+            MutableAcl acl = null;
+            try {
+                acl = (MutableAcl) this.aclService.readAclById(oid);
+            } catch (NotFoundException ex) {
+                acl = this.aclService.createAcl(oid);
+            } finally {
+                if (acl == null) {
+                    throw new RuntimeException("Unable to create ACL for Widget: " + w.getId());
+                }
+            }
+
+            // create required ACL entries for the Widget
+            // TODO need to find a way to read the current config and apply it to Widget
+
+        }
 }
